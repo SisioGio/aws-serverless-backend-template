3a1965e137cb0104783f3aed2fb8094d39221fab
SisioGio <alessiogiovannini23@gmail.com>
2025-11-15 11:34:57 +0100

    Modified gitignore

--- cdk.out/asset.03a29e6a79c557d4312d806fe1385159627bf184b52dfdc5401560a95e337671/private/handler.py
+++
@@ -1,7 +0,0 @@
-def handler(event, context):
-    # Simple test Lambda handler
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": '{"message": "Hello from private"}'
-    }
